70d75cf8b4b277197f5c77fd0a046944beb21ce5
smallwindmill <h1803405349@163.com>
2017-08-16 13:48:41 +0800

    first
    url、date

---
+++ common.js
@@ -0,0 +1,32 @@
+// 地址栏通过url获取参数值
+function GetQueryString(name)
+    {
+         var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+         var r = window.location.search.substr(1).match(reg);
+         if(r!=null) return unescape(r[2]); return null;
+    }
+    var classId=GetQueryString('classId');
+
+// 通过对Date原型链创建属性，将Date转化为统一的格式
+  Date.prototype.format = function(fmt) {
+     var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt)) {
+            fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    }
+     for(var k in o) {
+        if(new RegExp("("+ k +")").test(fmt)){
+             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+         }
+     }
+        return fmt;
+    }
+
+    var curr_Date=new Date().format('yyyy-MM-dd');
